8348a1c8a8d75ed5a8c3fe33105c30dfc1c3b861
AceHalili <acebyronehalili.startland@gmail.com>
2026-01-15 17:55:48 +0800

    feat: setup main card on input page

--- src/lib/pages/InputPage.svelte
+++ src/lib/pages/InputPage.svelte
@@ -1,44 +1,34 @@
-<script lang="ts">
-	import { Button, Separator } from "bits-ui";
+<script>
+	import ModeToggle from "$lib/components/ModeToggle.svelte";
+	import Button from "$lib/components/ui/button/button.svelte";
+	import { Menu } from "@lucide/svelte";
 </script>
 
 <div class="fixed inset-0 flex flex-col">
+	<!-- Main Content -->
 	<div class="flex-grow flex justify-center items-center">
 		<div
-			class="size-fit bg-surface border-line-bold rounded-xl border p-6 shadow-sm"
+			class="w-full max-w-3xl flex flex-col gap-2 bg-card border-border rounded-xl border p-6"
 		>
-			<h2 class="text-content text-xl font-bold">Hello Developer</h2>
-			<p class="text-content-muted mt-2">
-				This card automatically adjusts its colors when you switch themes.
-			</p>
-			<Button.Root
-				class="bg-primary hover:bg-primary-hover text-content-inverse mt-4 rounded-lg px-4 py-2 transition-colors"
-				onclick={() => {
-					// Cycle through light, dark, and sepia
-					const themes = ["light", "dark", "sepia"];
-					document.documentElement.setAttribute(
-						"data-theme",
-						themes[
-							(themes.indexOf(
-								document.documentElement.getAttribute("data-theme") || "light",
-							) +
-								1) %
-								themes.length
-						],
-					);
-				}}
-			>
-				Click Me
-			</Button.Root>
+			<div class="flex items-center justify-between">
+				<h1 class="text-2xl font-bold">ReadMeFast</h1>
+				<Button variant="outline" class="size-10">
+					<Menu />
+				</Button>
+			</div>
 		</div>
 	</div>
+
+	<!-- Footer -->
 	<div class="flex items-center justify-end gap-4 px-4 py-2">
 		<span>Made by HolyShaq</span>
-		<div class="h-full w-px bg-line-bold"></div>
+		<div class="h-full w-px bg-border"></div>
 		<span
 			>Source on <a href="https://github.com/HolyShaq/bits-ui">GitHub</a></span
 		>
-		<div class="h-full w-px bg-line-bold"></div>
+		<div class="h-full w-px bg-border"></div>
 		<span>Support me on <a href="https://ko-fi.com/holysaq">Ko-fi</a></span>
+		<div class="h-full w-px bg-border"></div>
+		<ModeToggle />
 	</div>
 </div>
