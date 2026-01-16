b8411e71deb16c0124ad3f0f623115b23f2f93b8
AceHalili <acebyronehalili.startland@gmail.com>
2026-01-16 16:48:04 +0800

    feat: add read page

---
+++ src/lib/pages/ReadPage.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Button } from "$lib/components/ui/button";
+	import { X } from "@lucide/svelte";
+</script>
+
+<div class="relative inset-0">
+	<Button
+		size="icon"
+		class="absolute top-8 left-8 text-accent-foreground hover:text-accent-foreground/80 hover:scale-110 transition-all duration-100 bg-background border-background hover:bg-background hover:border-background"
+	>
+		<X class="size-12" />
+	</Button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import InputPage from "$lib/pages/InputPage.svelte";
+	import ReadPage from "$lib/pages/ReadPage.svelte";
 
 	let text = $state("");
 </script>
 
-<InputPage bind:text />
+<!-- <InputPage bind:text /> -->
+<ReadPage />
