6deed1e98426520c4ab072fafa28077edadfbfbf
AceHalili <acebyronehalili.startland@gmail.com>
2026-02-02 14:03:04 +0800

    feat: add tooltip component

---
+++ src/lib/components/SettingsTooltip.svelte
@@ -0,0 +1,17 @@
+<script>
+  import * as Tooltip from "$lib/components/ui/tooltip/index.js";
+  import { CircleQuestionMark } from "@lucide/svelte";
+  import { buttonVariants } from "./ui/button";
+  let { text } = $props();
+</script>
+
+<Tooltip.Provider delayDuration={200}>
+  <Tooltip.Root>
+    <Tooltip.Trigger>
+      <CircleQuestionMark class="size-4" />
+    </Tooltip.Trigger>
+    <Tooltip.Content>
+      <p>{text}</p>
+    </Tooltip.Content>
+  </Tooltip.Root>
+</Tooltip.Provider>
